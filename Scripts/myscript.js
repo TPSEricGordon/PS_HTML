834ee5a17ecf8eef2f413862622133903f827cc2
Eric <eric.gordon@theprimaryschool.org>
2018-03-15 17:36:37 -0700

    STABLE updated Group Roster page, AngularJS has been included within the html file, and the json file is loading all of the data. There is still the problem with the WHERE clause in the tlist sql. Cannot pass the gpv.group_id value to compare in the where clause.

--- Scripts/myscript.js
+++ Scripts/myscript.js
@@ -1,16 +1,22 @@
-// this wraps all your code into Angular
+
 define(['angular', 'components/shared/index'], function (angular) { var myApp = angular.module('myModule', ['powerSchoolModule']);
-// this is your controller, Angular calls this function when the page is loaded // as it encounters the data­ng­controller directive
-myApp.controller('myController', function($scope, $http) {
-// $scope is a built­in Angular object. It contains everything that is bound // to the HTML elements. This line initializes an array within the $scope // object called “myData”
-$scope.myData = [];
-//$httpisabuilt­inAngularobject. Itcontainsallthemethodsfor // accessing web resources... such as this one: $http.get
-$http.get('parentData.json')
-// .then is executed after $http.get returns data ... it  gets the web page // (mydata.json)  then runs a function with the data from the web page placed // into the myJSON object
-.then(function(myJSON) {
-// .pop removes the last item in the array, which should be empty (see your // t_listsql)
-myJSON.data.pop();
-// copy the data array from myJSON to the $scope object variable, myData, which // is bound to the grid widget
-$scope.myData = myJSON.data; });
+
+	myApp.controller('parentSearchController', function($scope, $http) {
+			$scope.myData = [];
+			$http.get('parentData.json')
+			.then(function(myJSON) {
+					myJSON.data.pop();
+					$scope.myData = myJSON.data; });
+		});
+
+
+
+	myApp.controller('groupRosterController', function($scope, $http) {
+
+			$scope.myData = [];
+			$http.get('rosterData.json')
+			.then(function(myJSON) {
+					myJSON.data.pop();
+					$scope.myData = myJSON.data; });
+		});
 	});
-});
